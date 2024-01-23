085e2267d209fa97269b5eaad872082075a02df0
unknown <atharv21240@iiitd.ac.in>
2024-01-23 11:12:31 +0530

    sets

--- solve.cpp
+++ solve.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    priority_queue<int> pq;
-    pq.push(5);
-    pq.push(2);
-    pq.push(8);
-    pq.push(10);//{10,8,5,2}
-    pq.top();//10
-    pq.pop();//{8,5,2}
+    set<int> st;
+    st.insert(1);
+    st.insert(2);
+    st.insert(8);
+    st.insert(1);
+    st.insert(6);//{1,2,6,8}
+    // stores in sorted and unique elements only
 
-    // min heap
-    priority_queue<int, vector<int>,greater<int>> pq;
-    // prefers min element.
-    
+    auto it= st.find(3);//returns iterator that points to 3. returns st.end()
+    st.erase(6);//erase 6
+// multiset only have sorted, can store multiple numbers.
+    st.lower_bound(2);
+    st.upper_bound(4);
     return 0;
 }
