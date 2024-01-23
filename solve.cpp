4fa19e7c3dcdb3c7d9de7eb66aeac627b3564f18
unknown <atharv21240@iiitd.ac.in>
2024-01-23 08:39:46 +0530

    skeleton final

--- solve.cpp
+++ solve.cpp
@@ -1,4 +1,10 @@
-#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    cout<<arr[4];
     return 0;
 }
