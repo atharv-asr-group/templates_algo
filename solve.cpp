2b09ecb3e5f018387a09c00757c1dda4de9375f1
unknown <atharv21240@iiitd.ac.in>
2024-01-23 09:48:13 +0530

    stacks

--- solve.cpp
+++ solve.cpp
@@ -1,36 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    // vector is similar to array but is of dynamic size.
-    vector<int> v;
-    v.push_back(1);
-    v.emplace_back(2);
+    // every operation happens in constant time.
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.emplace(5);//{5,3,2,1}
 
-    vector<pair<int,int>>vec;
-    vec.push_back({1,2});
-    vec.emplace_back(1,2);
-
-    vector<int> v10(5,100);
-    // {100,100,100,100,100}
-    vector<int> v3(5);
-    vector<int> v2(v3);
-
-    cout<<v10[3];
-
-    vector<int>::iterator it=v.begin();
-    // v.begin points to the memory, not the value
-    it++;//memory location moved
-    cout<<*(it)<<' ';
-
-    vector<int>::iterator endit=v.end();
-    // v.end points to the memory just after the end of the vector, 
-    // so to access the last element we will have to do endit--;
-    // {10,20,30}v.erase takes iterator, this will delete the second element
-    v.erase(v.begin()+1);
-
-    v.pop_back();//{10,20} to {10}
-    v3.swap(v2);//the vectors will get interchanged.
-
-    v.clear();//erases the entire vector.
+    cout<<st.top();//5
+    st.pop();//{1,2,3}
     return 0;
 }
