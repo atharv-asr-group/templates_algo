0ee91589e620f44853444c02d5baca9c9926e009
unknown <atharv21240@iiitd.ac.in>
2024-01-23 11:00:37 +0530

    priority_queue

--- solve.cpp
+++ solve.cpp
@@ -1,12 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    queue<int> q;
-    q.push(1);
-    q.push(2);
-    q.emplace(4);//{1,2,4}
+    priority_queue<int> pq;
+    pq.push(5);
+    pq.push(2);
+    pq.push(8);
+    pq.push(10);//{10,8,5,2}
+    pq.top();//10
+    pq.pop();//{8,5,2}
 
-    q.back();//4
-    q.front();//1
+    // min heap
+    priority_queue<int, vector<int>,greater<int>> pq;
+    // prefers min element.
+    
     return 0;
 }
