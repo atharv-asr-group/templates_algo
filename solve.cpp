049967b2da56da1754dd4e6cbaa14f9ad3f3015d
unknown <atharv21240@iiitd.ac.in>
2024-01-23 09:20:44 +0530

    vectors

--- solve.cpp
+++ solve.cpp
@@ -1,10 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    pair<int,int> p={1,3};
-    cout<<p.first<<" "<<p.second;
-    pair<int,pair<int,int>> pp={1,{2,3}};
-    cout<<pp.first<<pp.second.first<<pp.second.second;
-    pair<int,int>arr[]={{1,2},{2,3},{4,5}};
+    // vector is similar to array but is of dynamic size.
+    vector<int> v;
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<pair<int,int>>vec;
+    vec.push_back({1,2});
+    vec.emplace_back(1,2);
+
+    vector<int> v10(5,100);
+    // {100,100,100,100,100}
+    vector<int> v3(5);
+    vector<int> v2(v3);
+
+    cout<<v10[3];
+
+    vector<int>::iterator it=v.begin();
+    // v.begin points to the memory, not the value
+    it++;//memory location moved
+    cout<<*(it)<<' ';
+
+    vector<int>::iterator endit=v.end();
+    // v.end points to the memory just after the end of the vector, 
+    // so to access the last element we will have to do endit--;
+    // {10,20,30}v.erase takes iterator, this will delete the second element
+    v.erase(v.begin()+1);
+
+    v.pop_back();//{10,20} to {10}
+    v3.swap(v2);//the vectors will get interchanged.
+
+    v.clear();//erases the entire vector.
     return 0;
 }
